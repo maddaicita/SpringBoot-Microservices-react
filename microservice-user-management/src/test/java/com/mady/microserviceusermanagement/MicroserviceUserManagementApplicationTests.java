db0672341ce308fd9b31e781fb3bb6b932c55853
Maday Moya <maday.moya001@mymdc.net>
2021-12-08 00:33:33 -0500

    First Microservice added

---
+++ microservice-user-management/src/test/java/com/mady/microserviceusermanagement/MicroserviceUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mady.microserviceusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
