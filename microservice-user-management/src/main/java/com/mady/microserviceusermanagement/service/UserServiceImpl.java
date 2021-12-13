e2e7cc0ebf4b8cfb79816757fcc7b50e427b3003
Maday Moya <maday.moya001@mymdc.net>
2021-12-13 05:33:00 -0500

    User service and impl added

---
+++ microservice-user-management/src/main/java/com/mady/microserviceusermanagement/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.mady.microserviceusermanagement.service;
+
+import com.mady.microserviceusermanagement.model.User;
+import com.mady.microserviceusermanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //We will create bean for it in security config.
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    @Override
+    public List<String> findUsers(List<Long> idList){
+        return userRepository.findByIdList(idList);
+    }
+}
