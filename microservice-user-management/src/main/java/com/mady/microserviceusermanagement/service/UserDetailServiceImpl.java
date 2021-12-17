20a3ce6d2790d7b32f123a63b10d60cf5dcc52f4
Maday Moya <maday.moya001@mymdc.net>
2021-12-17 01:07:18 -0500

    User Detail Service Impl added

--- microservice-user-management/src/main/java/com/mady/microserviceusermanagement/service/UserDetailServiceImpl.java
+++ microservice-user-management/src/main/java/com/mady/microserviceusermanagement/service/UserDetailServiceImpl.java
@@ -4,6 +4,7 @@ import com.mady.microserviceusermanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
