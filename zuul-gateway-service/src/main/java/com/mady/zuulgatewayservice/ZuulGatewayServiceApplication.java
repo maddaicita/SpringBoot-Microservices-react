5c5c780cdd4d9da921c7f186fdff3484f9da6e79
Maday Moya <maday.moya001@mymdc.net>
2022-01-27 00:48:01 -0500

    zuul-gateway-service added

--- zuul-gateway-service/src/main/java/com/mady/zuulgatewayservice/ZuulGatewayServiceApplication.java
+++ zuul-gateway-service/src/main/java/com/mady/zuulgatewayservice/ZuulGatewayServiceApplication.java
@@ -1,22 +1,21 @@
-package com.mady.microservicecoursemanagement;
+package com.mady.zuulgatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
-public class MicroserviceCourseManagementApplication {
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulGatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceCourseManagementApplication.class, args);
+		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
 	}
-
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
@@ -26,4 +25,5 @@ public class MicroserviceCourseManagementApplication {
 			}
 		};
 	}
+
 }
